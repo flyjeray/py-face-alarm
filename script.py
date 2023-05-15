de9b4d59a86241e1a426013295812c7dd57c74ff
Daniil Antoniuk <flyjeray.dev@gmail.com>
2023-05-15 10:22:39 +0300

    rewrite using threading to schedule events

--- script.py
+++ script.py
@@ -3,14 +3,14 @@ from gtts import gTTS
 from playsound import playsound
 import screen_brightness_control as sbc
 from os.path import exists
+from time import sleep
+from threading import Thread, Event
 
 cap = cv2.VideoCapture(0)
 
 # some variables.
-# person_here is switched when you enter/leave the screen
 # texts are what the Artificial Intelligence says
 # and sound_names are paths where mp3s are saved
-person_here = True
 goodbye_text = 'Goodbye'
 goodbye_sound_name = 'bye.mp3'
 hello_text = 'Hello'
@@ -31,36 +31,55 @@ if not exists(goodbye_sound_name):
 # get initial brightness
 initial_brightness = sbc.get_brightness()
 
-while(True):
-	# Capture frame-by-frame
-	ret, frame = cap.read()
-
-	# Our operations on the frame come here
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-	# Detect faces in the image
-	faces = faceCascade.detectMultiScale(
-		gray,
-		scaleFactor=1.1,
-		minNeighbors=5,
-		minSize=(30, 30)
-	)
+def screensave(event: Event):
+	playsound(goodbye_sound_name)
 	
-	# when you leave the screen, kill brightness and say goodbye
-	if len(faces) == 0 and person_here:
-		initial_brightness = sbc.get_brightness()
-		sbc.set_brightness(0)
-		playsound(goodbye_sound_name)
-		person_here = False
-	# when you come back, say hi and restore brightness
-	elif len(faces) > 0 and not person_here:
-		sbc.set_brightness(initial_brightness[0])
-		playsound(hello_sound_name)
-		person_here = True
+	sleep(3)
+	if (event.is_set()):
+		return
+	sbc.set_brightness(0)
 	
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+	sleep(5)
+	if (event.is_set()):
+		return
+	
+def main():
+	person_here = True
+	thread = None
+	event = Event()
+	
+	while True:
+		# Capture frame-by-frame
+		ret, frame = cap.read()
+	
+		# Our operations on the frame come here
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	
+		# Detect faces in the image
+		faces = faceCascade.detectMultiScale(
+			gray,
+			scaleFactor=1.1,
+			minNeighbors=5,
+			minSize=(30, 30)
+		)
+		
+		# when you leave the screen, start screensaver
+		if len(faces) == 0 and person_here:
+			event.clear()
+			thread = Thread(target=screensave, args=(event, ))
+			thread.start()
+			person_here = False
+		# when you come back, say hi and restore brightness
+		elif len(faces) > 0 and not person_here:
+			event.set()
+			playsound(hello_sound_name)
+			sbc.set_brightness(initial_brightness[0])
+			person_here = True
+			
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+	# When everything done, release the capture
+	cap.release()
+	cv2.destroyAllWindows()
+	
+main()
